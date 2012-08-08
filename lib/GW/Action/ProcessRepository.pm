ceaaba0ccbebe350b7ca7ee86a03e6c624da965d
Wakaba <wakabatan@hatena.ne.jp>
2012-08-08 18:34:37 +0900

    repository action

---
+++ lib/GW/Action/ProcessRepository.pm
@@ -0,0 +1,110 @@
+package GW::Action::ProcessRepository;
+use strict;
+use warnings;
+use File::Temp;
+use Path::Class;
+use AnyEvent;
+use AnyEvent::Util;
+
+my $DEBUG = $ENV{GW_DEBUG};
+
+sub new_from_job {
+    return bless {job => $_[1]}, $_[0];
+}
+
+sub url {
+    return $_[0]->{job}->{repository_url};
+}
+
+sub branch {
+    return $_[0]->{job}->{repository_branch};
+}
+
+sub revision {
+    return $_[0]->{job}->{repository_revision};
+}
+
+sub onmessage {
+    if (@_ > 1) {
+        $_[0]->{onmessage} = $_[1];
+    }
+    return $_[0]->{onmessage} ||= sub { };
+}
+
+sub print_message {
+    my ($self, $msg) = @_;
+    $self->onmessage->($msg);
+}
+
+sub die_message {
+    my ($self, $msg) = @_;
+    $self->onmessage->($msg, die => 1);
+    die $msg;
+}
+
+sub temp_repo_temp {
+    my $self = shift;
+    return $self->{temp_repo_temp} ||= File::Temp->newdir(CLEANUP => !$DEBUG);
+}
+
+sub temp_repo_d {
+    my $self = shift;
+    return $self->{temp_repo_d} ||= dir($self->temp_repo_temp->dirname);
+}
+
+sub git_as_cv {
+    my ($self, $cmd, %args) = @_;
+    $self->print_message('$ ' . join ' ', 'git', @$cmd);
+    my $onmessage = $self->onmessage;
+    if ($cmd->[0] ne 'clone') {
+        $cmd = ['sh', '-c', 'cd ' . (quotemeta $self->temp_repo_d) . ' && git ' . join ' ', map { quotemeta } @$cmd];
+    } else {
+        $cmd = ['git', @$cmd];
+    }
+    return run_cmd $cmd, 
+        '>' => $args{onstdout} || sub {
+            $onmessage->($_[0]) if defined $_[0];
+        },
+        '2>' => sub {
+            $onmessage->($_[0]) if defined $_[0];
+        },
+    ;
+}
+
+sub clone_depth {
+    return 200;
+}
+
+sub clone_as_cv {
+    my $self = shift;
+    my $cv = AE::cv;
+    my $clone_cv = $self->git_as_cv(['clone', '--depth' => $self->clone_depth, $self->url => $self->temp_repo_d->stringify])->cb(sub {
+        $self->git_as_cv(['checkout', $self->revision])->cb(sub {
+            $self->git_as_cv(['submodule', 'update', '--init'])->cb(sub {
+                $cv->send;
+            });
+        });
+    });
+    return $cv;
+}
+
+sub run_action_as_cv {
+    my $self = shift;
+    my $action = $self->{job}->{action_type};
+    my $args = $self->{job}->{args};
+
+    my $cv = AE::cv;
+    $self->clone_as_cv->cb(sub {
+        if ($action eq 'make') {
+            my $run_cv = run_cmd
+                "cd @{[quotemeta $self->temp_repo_d]} && (make @{[quotemeta $args->{rule}]})";
+            $run_cv->cb(sub { $cv->send });
+        } else {
+            warn "Action |$action| is not supported";
+            $cv->send;
+        }
+    });
+    return $cv;
+}
+
+1;
---
+++ t/action/processrepository.t
@@ -0,0 +1,65 @@
+use strict;
+BEGIN {
+    my $dir_name = __FILE__; $dir_name =~ s{[^/]+$}{}; $dir_name ||= '.';
+    $dir_name .= '/../lib'; unshift @INC, $dir_name;
+}
+use warnings;
+use Test::GW;
+use Test::X1;
+use Test::More;
+use File::Temp qw(tempdir);
+use Path::Class;
+use GW::Action::ProcessRepository;
+
+test {
+    my $c = shift;
+
+    my $temp_d = dir(tempdir(CLEANUP => 1));
+    system "cd $temp_d && git init && echo 'hoge:\n\techo 1234 > foo.txt' > Makefile && git add Makefile && git commit -m New";
+    my $rev = `cd $temp_d && git rev-parse HEAD`;
+
+    my $job = {
+        repository_url => $temp_d->stringify,
+        repository_branch => 'master',
+        repository_revision => $rev,
+        action_type => 'make',
+        args => {
+            rule => 'hoge',
+        },
+    };
+    my $action = GW::Action::ProcessRepository->new_from_job($job);
+    $action->run_action_as_cv->cb(sub {
+        test {
+            is scalar $action->temp_repo_d->file('foo.txt')->slurp, "1234\n";
+            done $c;
+        } $c;
+    });
+} n => 1;
+
+test {
+    my $c = shift;
+
+    my $temp_d = dir(tempdir(CLEANUP => 1));
+    system "cd $temp_d && git init && echo 'hoge:\n\techo 1234 > foo.txt' > Makefile && git add Makefile && git commit -m New";
+    my $rev = `cd $temp_d && git rev-parse HEAD`;
+    system "cd $temp_d && echo 'hoge:\n\techo 9999 > foo.txt' > Makefile && git add Makefile && git commit -m Old";
+
+    my $job = {
+        repository_url => $temp_d->stringify,
+        repository_branch => 'master',
+        repository_revision => $rev,
+        action_type => 'make',
+        args => {
+            rule => 'hoge',
+        },
+    };
+    my $action = GW::Action::ProcessRepository->new_from_job($job);
+    $action->run_action_as_cv->cb(sub {
+        test {
+            is scalar $action->temp_repo_d->file('foo.txt')->slurp, "1234\n";
+            done $c;
+        } $c;
+    });
+} n => 1;
+
+run_tests;
