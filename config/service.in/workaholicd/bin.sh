4ec14789b3dee0e2a08dc7639d0ace555be0ff01
Wakaba <wakabatan@hatena.ne.jp>
2012-08-13 20:00:13 +0900

    config

--- config/service.in/workaholicd/bin.sh
+++ config/service.in/workaholicd/bin.sh
@@ -6,4 +6,4 @@ export GW_DSNS_JSON=@@INSTANCECONFIG@@/dsns.json
 export GW_API_KEY_FILE_NAME=@@INSTANCECONFIG@@/apikey.txt
 export GW_WEB_HOSTNAME=localhost
 export GW_WEB_PORT=@@PORT@@
-exec setuidgid @@USER@@ perl @@ROOT@@/bin/workaholicd.pl
+exec setuidgid @@USER@@ perl @@ROOT@@/bin/workaholicd.pl @@ROOT@@/config/workaholicd.pl
