2949f2e9cde638bad0ce11d37c696d0a8b258aee
Wakaba <wakabatan@hatena.ne.jp>
2012-10-16 17:32:45 +0900

    debug

--- config/service.in/web/bin.sh
+++ config/service.in/web/bin.sh
@@ -7,6 +7,7 @@ export GW_API_KEY_FILE_NAME=@@INSTANCECONFIG@@/apikey.txt
 export GW_COMMAND_DIR_NAME=@@INSTANCECONFIG@@/commands
 export KARASUMA_CONFIG_JSON=@@INSTANCECONFIG@@/@@INSTANCENAME@@.json
 export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
+export WEBUA_DEBUG=2
 export HOME=@@LOCAL@@/home
 exec setuidgid @@USER@@ perl `which plackup` $PLACK_COMMAND_LINE_ARGS \
     -p @@PORT@@ @@ROOT@@/bin/server.psgi
