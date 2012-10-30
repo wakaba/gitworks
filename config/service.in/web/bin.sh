c01b4062162404dfee50801ec31bde6aa7db8df8
Wakaba <wakabatan@hatena.ne.jp>
2012-10-30 20:29:45 +0900

    service

--- config/service.in/web/bin.sh
+++ config/service.in/web/bin.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 exec 2>&1
-export PATH=@@LOCAL@@/perl-latest/pm/bin:@@LOCAL@@/perlbrew/perls/perl-latest/bin:${PATH}
-export PERL5LIB=`cat @@ROOT@@/config/perl/libs.txt`
 export GW_DSNS_JSON=@@INSTANCECONFIG@@/dsns.json
 export GW_API_KEY_FILE_NAME=@@INSTANCECONFIG@@/apikey.txt
 export GW_COMMAND_DIR_NAME=@@INSTANCECONFIG@@/commands
 export KARASUMA_CONFIG_JSON=@@INSTANCECONFIG@@/@@INSTANCENAME@@.json
 export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
-export WEBUA_DEBUG=2
 export HOME=@@LOCAL@@/home
-exec setuidgid @@USER@@ perl `which plackup` $PLACK_COMMAND_LINE_ARGS \
+
+export WEBUA_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.WEBUA_DEBUG text`
+export SQL_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.SQL_DEBUG text`
+port=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl gitworks.web.port text`
+
+exec setuidgid @@USER@@ @@ROOT@@/plackup $PLACK_COMMAND_LINE_ARGS \
     -p @@PORT@@ @@ROOT@@/bin/server.psgi
--- config/service.in/workaholicd/bin.sh
+++ config/service.in/workaholicd/bin.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 exec 2>&1
-export PATH=@@LOCAL@@/perl-latest/pm/bin:@@LOCAL@@/perlbrew/perls/perl-latest/bin:${PATH}
-export PERL5LIB=`cat @@ROOT@@/config/perl/libs.txt`
 export GW_DSNS_JSON=@@INSTANCECONFIG@@/dsns.json
 export GW_API_KEY_FILE_NAME=@@INSTANCECONFIG@@/apikey.txt
-export GW_WEB_HOSTNAME=localhost
-export GW_WEB_PORT=@@PORT@@
-export WEBUA_DEBUG=2
 export HOME=@@LOCAL@@/home
-exec setuidgid @@USER@@ perl @@ROOT@@/bin/workaholicd.pl @@ROOT@@/config/workaholicd.pl
+
+export WEBUA_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.WEBUA_DEBUG text`
+export SQL_DEBUG=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl env.SQL_DEBUG text`
+port=`@@ROOT@@/perl @@ROOT@@/modules/karasuma-config/bin/get-json-config.pl gitworks.web.port text`
+
+export GW_WEB_HOSTNAME=localhost
+export GW_WEB_PORT=$port
+
+exec setuidgid @@USER@@ @@ROOT@@/perl @@ROOT@@/bin/workaholicd.pl @@ROOT@@/config/workaholicd.pl
